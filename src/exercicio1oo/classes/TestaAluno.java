70727fd5509d5701c9b4e100feaa6c3aab2268e6
Kátila Stefanes <katilastefanes23@gmail.com>
2026-09-17 21:45:04 -0300

    classe Aluno

---
+++ src/exercicio1oo/classes/TestaAluno.java
@@ -0,0 +1,28 @@
+package exercicio1oo.classes;
+
+public class TestaAluno {
+    static void main(String[] args) {
+        Aluno katila = new Aluno();
+        katila.matricula = "14100033";
+        katila.nome = "Kátila Thaiana Stefanes";
+        katila.idade = 30;
+        katila.nota1 = 10;
+        katila.nota2 = 10;
+        katila.nota3 = 9;
+        katila.nota4 = 10;
+        System.out.println("Matrícula: " + katila.matricula);
+        System.out.println("Nome: " + katila.nome);
+        System.out.println("Idade: " + katila.idade);
+        System.out.println("Nota 1: " + katila.nota1);
+        System.out.println("Nota 2: " + katila.nota2);
+        System.out.println("Nota 3: " + katila.nota3);
+        System.out.println("Nota 4: " + katila.nota4);
+
+
+
+
+
+
+
+    }
+}
